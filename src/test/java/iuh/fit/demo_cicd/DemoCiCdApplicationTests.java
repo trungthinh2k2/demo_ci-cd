6b2d9833b629a6f1b5dc07d2a4cdf202df521647
trungthinh2k2 <89030667+trungthinh2k2@users.noreply.github.com>
2024-10-16 13:43:16 +0700

    create project

---
+++ src/test/java/iuh/fit/demo_cicd/DemoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.demo_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
